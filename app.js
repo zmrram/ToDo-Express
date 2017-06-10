bb61216f5ff884dc365e10cabbe6dab4b9ff923e
Tin Huynh <Tin Huynh>
2017-06-09 21:58:28 -0700

    fix typo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-const session = require('express-sessoion');
+const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 
