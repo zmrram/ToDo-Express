335075cf18a21e351bc5d03950eb42545dccb41b
Tin Huynh <Tin Huynh>
2017-06-09 23:16:39 -0700

    successfully grab info from register post request

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,8 @@ var router = express.Router();
 router.get('/register', function(req, res) {
     res.render('register');
 });
+
+router.post('/register', function(req, res) {
+    console.log(req.body);
+})
 module.exports = router;
